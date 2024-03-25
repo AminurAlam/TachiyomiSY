d6bfaf17f350e9a4ebbbef24bb14249833df2682
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-03-25 15:33:01 +0530

    modify lavender theme

--- app/src/main/java/eu/kanade/presentation/theme/colorscheme/LavenderColorScheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/colorscheme/LavenderColorScheme.kt
@@ -9,37 +9,37 @@ import androidx.compose.ui.graphics.Color
  * Color scheme by Osyx
  *
  * Key colors:
- * Primary #A177FF
- * Secondary #A177FF
- * Tertiary #5E25E1
- * Neutral #111129
+ * Primary #7DCFFF
+ * Secondary #7DCFFF
+ * Tertiary #9D7CD8
+ * Neutral #24283B
  */
 internal object LavenderColorScheme : BaseColorScheme() {
 
     override val darkScheme = darkColorScheme(
-        primary = Color(0xFFA177FF),
-        onPrimary = Color(0xFF111129),
-        primaryContainer = Color(0xFFA177FF),
-        onPrimaryContainer = Color(0xFF111129),
-        inversePrimary = Color(0xFF006D2F),
-        secondary = Color(0xFFA177FF),
-        onSecondary = Color(0xFF111129),
-        secondaryContainer = Color(0xFFA177FF),
-        onSecondaryContainer = Color(0xFF111129),
-        tertiary = Color(0xFF5E25E1),
-        onTertiary = Color(0xFFE8E8E8),
-        tertiaryContainer = Color(0xFF111129),
-        onTertiaryContainer = Color(0xFFDEE8FF),
-        background = Color(0xFF111129),
-        onBackground = Color(0xFFDEE8FF),
-        surface = Color(0xFF111129),
-        onSurface = Color(0xFFDEE8FF),
-        surfaceVariant = Color(0x2CB6B6B6),
-        onSurfaceVariant = Color(0xFFE8E8E8),
-        surfaceTint = Color(0xFFA177FF),
+        primary = Color(0xFF7DCFFF),
+        onPrimary = Color(0xFF24283B),
+        primaryContainer = Color(0xFF7DCFFF),
+        onPrimaryContainer = Color(0xFF24283B),
+        inversePrimary = Color(0xFF5AF79E),
+        secondary = Color(0xFF7DCFFF),
+        onSecondary = Color(0xFF24283B),
+        secondaryContainer = Color(0xFF7DCFFF),
+        onSecondaryContainer = Color(0xFF24283B),
+        tertiary = Color(0xFF9D7CD8),
+        onTertiary = Color(0xFFF8F8F2),
+        tertiaryContainer = Color(0xFF24283B),
+        onTertiaryContainer = Color(0xFFC0CAF5),
+        background = Color(0xFF24283B),
+        onBackground = Color(0xFFC0CAF5),
+        surface = Color(0xFF24283B),
+        onSurface = Color(0xFFC0CAF5),
+        surfaceVariant = Color(0x2CE6E6E6),
+        onSurfaceVariant = Color(0xFFF8F8F2),
+        surfaceTint = Color(0xFF7DCFFF),
         inverseSurface = Color(0xFF221247),
-        inverseOnSurface = Color(0xFFDEE8FF),
-        outline = Color(0xA8905FFF),
+        inverseOnSurface = Color(0xFFC0CAF5),
+        outline = Color(0xA8CAA9FA),
     )
 
     override val lightScheme = lightColorScheme(
